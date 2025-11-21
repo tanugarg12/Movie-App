36ca10489b0895a109313efb8b50b9e5d3c73e67
tanugarg12 <tanugarg5506@gmail.com>
2025-11-22 02:58:22 +0530

    updated import path

--- movie/src/Components/MovieDetails.jsx
+++ movie/src/Components/MovieDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { asyncloadmovie, removemovie } from "../store/actions/MovieActions";
+import { asyncloadmovie, removemovie } from "../store/actions/movieActions";
 import { useNavigate, useParams, Link, useLocation, Outlet } from "react-router-dom";
 import Loading from "../Components/Loading";
 import HorizontalCards from "./partials/HorizontalCards";
